4902c038dffcc2d9465384c2925ebc141711a84a
Enzhy58 <ang.borisova2015@yandex.ru>
2022-07-18 23:22:44 +0300

    fix services section, fix request section

--- app/js/main.js
+++ app/js/main.js
@@ -59,6 +59,8 @@ $(function(){
     ]
   });
 
+  $('.select-style').styler();
+
 
 });
 
--- gulpfile.js
+++ gulpfile.js
@@ -64,6 +64,7 @@ function scripts() {
     'node_modules/jquery/dist/jquery.js',
     'node_modules/slick-carousel/slick/slick.js',
     'node_modules/swiper/swiper-bundle.js',
+    'node_modules/jquery-form-styler/dist/jquery.formstyler.js',
     'app/js/main.js'
   ])
     .pipe(concat('main.min.js'))
